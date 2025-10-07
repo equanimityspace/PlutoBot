db0eed7d4f5682876b49e0cd365ba1bf870f340b
ryan <ryfly2016@gmail.com>
2025-10-07 17:06:41 -0600

    create simple text generation function with guardrails on thinking and token use that the discord bot can output

---
+++ gentext.py
@@ -0,0 +1,26 @@
+from google import genai
+from google.genai import types
+import settings
+
+client = genai.Client(api_key=settings.google_ai_token)
+
+async def gen_text(prompt):
+    response = client.models.generate_content(
+        model="gemini-flash-latest",
+        contents=prompt,
+        # turn off "thinking" feature for testing
+        # TODO enable thinking, consider budget https://ai.google.dev/gemini-api/docs/thinking
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=0), # dynamic thinking: thinking_budget=-1
+            # attempt to restrict length of responses. 1 token is 3-4 words, so restrict to 500 words max (hopefully under 4000 characters)
+            max_output_tokens=400,
+            # global prompt
+            # TODO allow users to choose individual system prompts?
+            system_instruction="""
+            You are a discord user. You format messages as if they are discord messages. You are appropriately brief with your responses,
+            but you still make a good-faith effort to be helpful. However, you do not know everything. You will format your responses as
+            though they are discord messages, infrequently making use of emojis, and occassionally reference internet culture.
+            """
+        )
+    )
+    return response.text
