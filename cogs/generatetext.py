3360bde47f09703a9321b16e68c39a73f1df3297
ryan <ryfly2016@gmail.com>
2025-10-23 18:23:32 -0600

    give bot improved context by sending it most recent 20 messages

--- cogs/generatetext.py
+++ cogs/generatetext.py
@@ -4,14 +4,29 @@ from gentext import gen_text
 class GenerateText(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
+    # prevent bot from replying to itself        
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
              return
 
         if f'<@{self.bot.user.id}>' in message.content:
-            response = await gen_text(message.content)
+            # first, grab most recent 20 messages in that channel
+            channel = self.bot.get_channel(message.channel.id)
+            history_data = [past_message async for past_message in channel.history(limit=20)]
+            
+            # history currently has all data on every message, get author id and message
+            history = []
+            for msg in history_data:
+                past_message = await channel.fetch_message(msg.id)
+                history.append({
+                    "author_id": msg.author.id,
+                    "message": past_message.content
+                })
+            
+            # send prompt and message history to bot
+            response = await gen_text(f'user prompt: {message.content}\nlist of past 20 messages in channel:\n{history}')
             await message.reply(response)
 
 async def setup(bot):
